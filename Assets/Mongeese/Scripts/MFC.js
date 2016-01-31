6793e96cb92c075fe3eddb65faef803bb3358708
TomLong74 <tomlong74@gmail.com>
2016-01-30 19:59:56 -0500

    FSM and JS code - tom

---
+++ Assets/Mongeese/Scripts/MFC.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+private gameState : string = "enter"
+
+//Start
+function Start () {
+
+}
+
+//Update
+function Update () {
+	FSM(gameState);
+}
+
+//FSM
+function FSM (gameState: string) {
+	switch {
+		case:"enter"
+				break;
+		case:"execute"
+				break;
+		case:"exit"
+				break;
+	}
+}
